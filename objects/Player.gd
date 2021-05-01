87b623edf8ac270381b9d7c93f43fd4bfaad7424
Leon Sandøy <leon.sandoy@conmodo.com>
2021-05-01 17:42:27 +0200

    Export some Player variables.

--- objects/Player.gd
+++ objects/Player.gd
@@ -3,14 +3,17 @@ This script controls the player character.
 """
 extends KinematicBody2D
 
-const GRAVITY = 20
-const MAX_SPEED = 1000
-const FRICTION_AIR = 0.95
-const FRICTION_GROUND = 0.85
-const TONGUE_PULL = 25
+export var gravity = 20
+export var max_speed = 1000
+export var friction_air = 0.95
+export var tongue_pull = 25
 
+var friction_ground = 0.85
 var velocity = Vector2(0,0)
 var tongue_velocity = Vector2(0,0)
+var is_flipping = false
+var is_initialized = false
+var flip_speed = 2
 
 
 func _input(event: InputEvent) -> void:
@@ -26,11 +29,13 @@ func _input(event: InputEvent) -> void:
 
 func _physics_process(_delta: float) -> void:
 
-	velocity.y += GRAVITY
+	# Gravity is a thing that exists.
+	velocity.y += gravity
 
+	# Slurp physics!
 	if $Tongue.hooked:
 		var tongue_direction = to_local($Tongue.tip_location).normalized() 
-		tongue_velocity = tongue_direction * TONGUE_PULL
+		tongue_velocity = tongue_direction * tongue_pull
 		
 		if tongue_velocity.y > 0:
 			tongue_velocity.y *= 0.55
@@ -46,21 +51,27 @@ func _physics_process(_delta: float) -> void:
 	move_and_slide(velocity, Vector2.UP)
 	
 	# Apply some clamps. We don't want the frog moving _too_ fast.
-	velocity.y = clamp(velocity.y, -MAX_SPEED, MAX_SPEED)
-	velocity.x = clamp(velocity.x, -MAX_SPEED, MAX_SPEED)
+	velocity.y = clamp(velocity.y, -max_speed, max_speed)
+	velocity.x = clamp(velocity.x, -max_speed, max_speed)
 	
 	# Apply surface friction
-	var grounded = is_on_floor()
 	if grounded:
-		velocity.x *= FRICTION_GROUND	# Apply friction only on x (we are not moving on y anyway)
+		
+		# Is this the first time?
+		if not self.is_initialized:
+			self.is_initialized = true
+		
+		$Sprite.rotation = 0
+		velocity.x *= friction_ground	# Apply friction only on x (we are not moving on y anyway)
 		if velocity.y >= 5:		        # Keep the y-velocity small such that
 			velocity.y = 5		        # gravity doesn't make this number huge
 			
 	elif is_on_ceiling() and velocity.y <= -5:
 		velocity.y = -5
 
-	# Apply air friction
-	if !grounded:
-		velocity.x *= FRICTION_AIR
+	if not grounded:
+		# Apply air friction		
+		velocity.x *= friction_air
 		if velocity.y > 0:
-			velocity.y *= FRICTION_AIR
+			velocity.y *= friction_air
+			
